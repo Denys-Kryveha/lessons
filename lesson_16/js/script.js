afccc75b29d21cc09f6badeb5f9d7f6864163168
Denys-Kryveha <103812661+Denys-Kryveha@users.noreply.github.com>
2024-06-12 10:07:17 +0300

    Delete lesson_16 directory

--- lesson_16/js/script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.mobile-header__menu')) {
-		document.documentElement.classList.toggle('mobile-header__menu--open');
-	}
-})
